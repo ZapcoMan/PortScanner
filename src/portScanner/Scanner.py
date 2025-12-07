800783eb0b8b3be42e0e02519f83065ff1f2e226
codervibe <3217277135@qq.com>
2025-12-08 00:14:30 +0800

    feat(scanner): 实现端口扫描器模块化与功能优化
    - 新增 banner 模块，统一程序启动横幅显示
    - 重构 Scanner 类，支持命令行参数解析和多线程扫描
    - 添加 CDN 检测逻辑，避免对使用 CDN 的目标进行无效扫描
    - 支持 HTTP/HTTPS 协议的 banner 识别及 socket 信息获取
    - 优化端口扫描性能，增加超时控制和异常处理机制
    - 提升用户交互体验，完善扫描结果展示与中断处理

--- src/portScanner/Scanner.py
+++ src/portScanner/Scanner.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-#coding:utf-8
-#Author:se55i0n
-#目标tcp端口开放扫描及应用端口banner识别
-
 import sys
 import socket
 import logging
@@ -13,19 +8,14 @@ import argparse
 from time import time
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-# 线程池
+#线程池
 from multiprocessing.dummy import Pool as ThreadPool
 from multiprocessing.dummy import Lock
-# 导入banner
-from src.portScanner.banner import print_banner
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-logger = logging.getLogger(__name__)
 
 class Scanner(object):
-	def __init__(self, target, start, end, threads=100):
-		self.target = target
+	def __init__(self, url, start, end, threads=100):
+		self.url = url
 		self.start  = start
 		self.end    = end
 		self.threads = threads
@@ -51,7 +41,7 @@ class Scanner(object):
 		try:
 			for i in dnsserver:
 				myResolver.nameservers = i
-				record = myResolver.resolve(self.target)
+				record = myResolver.resolve(self.url)
 				self.result.append(record[0].address)
 		except Exception as e:
 			pass
@@ -63,7 +53,7 @@ class Scanner(object):
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.settimeout(0.2)
-			return True if s.connect_ex((self.target, port)) == 0 else False
+			return True if s.connect_ex((self.url, port)) == 0 else False
 		except Exception as e:
 			pass
 		finally:
@@ -72,7 +62,7 @@ class Scanner(object):
 	def get_http_banner(self, url):
 		#http/https请求获取banner
 		try:
-			r = requests.get(url, headers={'UserAgent':UserAgent().random}, 
+			r = requests.get(url, headers={'UserAgent':UserAgent().random},
 				timeout=2, verify=False, allow_redirects=True)
 			soup = BeautifulSoup(r.content,'lxml')
 			return soup.title.text.strip('\n').strip()
@@ -84,7 +74,7 @@ class Scanner(object):
 		try:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			s.settimeout(0.2)
-			s.connect((self.target, port))
+			s.connect((self.url, port))
 			s.send('HELLO\r\n')
 			return s.recv(1024).split('\r\n')[0].strip('\r\n')
 		except Exception as e:
@@ -95,14 +85,14 @@ class Scanner(object):
 	def run(self, port):
 		try:
 			if self.scan_port(port):
-				banner = self.get_http_banner('http://{}:{}'.format(self.target,port))
+				banner = self.get_http_banner('http://{}:{}'.format(self.url, port))
 				self.mutex.acquire()
-				if banner:					
+				if banner:
 					print('{}[+] {} ---- open   {}{}'.format(self.G,
 						str(port).rjust(6), banner[:18], self.W))
 				else:
 					banner = self.get_http_banner('https://{}:{}'.format(
-						self.target, port))
+						self.url, port))
 					if banner:
 						print('{}[+] {} ---- open   {}{}'.format(self.G,
 							str(port).rjust(6), banner[:18], self.W))
@@ -121,8 +111,8 @@ class Scanner(object):
 	def _start(self):
 		try:
 			print('-'*60)
-			print(u'{}[-] 正在扫描地址: {}{} '.format(self.O, 
-				socket.gethostbyname(self.target), self.W))
+			print(u'{}[-] 正在扫描地址: {}{} '.format(self.O,
+                                                      socket.gethostbyname(self.url), self.W))
 			print('-'*60)
 			#线程数
 			pool = ThreadPool(processes=self.threads)
@@ -131,7 +121,7 @@ class Scanner(object):
 			pool.close()
 			pool.join()
 			print('-'*60)
-			print(u'{}[-] 扫描完成耗时: {} 秒.{}'.format(self.O, 
+			print(u'{}[-] 扫描完成耗时: {} 秒.{}'.format(self.O,
 				time()-self.time, self.W))
 		except KeyboardInterrupt:
 			# 修改键盘中断处理逻辑，确保程序可以快速结束
@@ -142,7 +132,7 @@ class Scanner(object):
 
 	def check_target(self):
 		#判断目标是域名还是还是ip地址
-		flag = self.target.split('.')[-1]
+		flag = self.url.split('.')[-1]
 		try:
 			#ip地址
 			if int(flag) >= 0:
@@ -152,44 +142,6 @@ class Scanner(object):
 			if not self.check_cdn():
 				self._start()
 			else:
-				print('-'*60)	
+				print('-'*60)
 				print(u'{}[-] 目标使用了CDN技术,停止扫描.{}'.format(self.R, self.W))
 				print('-'*60)
-
-def parse_args():
-	parser = argparse.ArgumentParser(description='TCP端口扫描工具')
-	parser.add_argument('-u', '--url', required=True, help='目标IP地址或域名')
-	parser.add_argument('-p', '--port', required=True, help='端口范围 (例如: 21-8080 或 80)')
-	parser.add_argument('-T', '--threads', type=int, default=100, help='线程数 (默认: 100)')
-	parser.add_argument('-v', '--version', action='version', version='%(prog)s 1.0')
-	return parser.parse_args()
-
-def parse_port_range(port_str):
-	"""解析端口范围字符串"""
-	if '-' in port_str:
-		start, end = port_str.split('-', 1)
-		return int(start), int(end)
-	else:
-		port = int(port_str)
-		return port, port
-
-if __name__ == '__main__':
-	print_banner()
-	
-	# // 删除旧的使用方式，只保留命令行参数解析
-	try:
-		# // 解析命令行参数
-		args = parse_args()
-		
-		# // 解析端口范围
-		start_port, end_port = parse_port_range(args.port)
-		
-		# // 创建扫描器实例
-		myscan = Scanner(args.url, start_port, end_port, args.threads)
-		myscan.check_target()
-	except KeyboardInterrupt:
-		print("\nExit...")
-		sys.exit(0)
-	except Exception as e:
-		print("Error:", str(e))
-		sys.exit(1)
---
+++ src/portScanner/__init__.py
@@ -0,0 +1,3 @@
+from .banner import BANNER, print_banner
+
+__all__ = ['BANNER', 'print_banner']
